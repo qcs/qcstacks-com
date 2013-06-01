cbb098cc1861f82888fa2177b12b0e84f5b5a426
Taylor C. MacDonald <tcmacdonald@gmail.com>
2013-05-31 23:11:41 -0400

    Cleanup index and feed content.

--- config.rb
+++ config.rb
@@ -3,33 +3,23 @@
 require 'compass'
 require 'susy'
 
-
 ###---------------------------------------------------- Blog Settings
 
 activate :blog do |blog|
-  # blog.prefix = "blog"
   blog.permalink = ":year/:month/:day/:title.html"
   blog.sources = ":year/:month/:day/:title.html"
-  # blog.taglink = "tags/:tag.html"
   blog.layout = "article"
   blog.summary_separator = /(READMORE)/
-  # blog.year_link = ":year.html"
-  # blog.month_link = ":year/:month.html"
-  # blog.day_link = ":year/:month/:day.html"
   blog.default_extension = ".erb"
   blog.paginate = false
   blog.calendar_template = "calendar.html"
-  # blog.per_page = 10
-  # blog.page_link = "page/:num"
 end
-
 
 ###---------------------------------------------------- Layout Settings
 
 page "/", layout: "public"
 page "/archive/*", layout: "public"
 page "/about", layout: "public"
-page "/sign-up", layout: "public"
 page "/email/*", layout: false
 page "/feed.xml", layout: false
 
@@ -41,13 +31,12 @@ ready do
   page "/archive/index.html", :layout => :public do
     @archives = archive_resources
   end
- end
-
+end
 
 ###---------------------------------------------------- Helper Methods
 
 helpers do
-  # Calculate the years for a copyright
+
   def copyright_years(start_year)
     end_year = Date.today.year
     if start_year == end_year
@@ -58,41 +47,27 @@ helpers do
   end
 
   def recent_articles
-    @recent_articles ||= blog.articles.collect{ |a| a if a.date <= Date.today }.compact.group_by{ |a| a.date }.take(5)
+    @recent_articles ||= blog.articles.collect{ |a| a if a.date <= Date.today }.compact
+  end
+
+  def recent_articles_grouped
+    @recent_articles_grouped ||= recent_articles.group_by{|a| a.date }
   end
 
   def todays_date
     @todays_date ||= Date.today.strftime("%A, %B %e %Y")
   end
 
-  def month(month)
-    
-  end
 end
 
 
 ###---------------------------------------------------- Build Settings
 
 configure :build do
-  # For example, change the Compass output style for deployment
   activate :minify_css
-  
-  # Minify Javascript on build
   activate :minify_javascript
-  
-  # Enable cache buster
   activate :cache_buster
-  
-  # Use relative URLs
   activate :relative_assets
-  
-  # Compress PNGs after build
-  # First: gem install middleman-smusher
-  # require "middleman-smusher"
-  # activate :smusher
-  
-  # Or use a different image path
-  # set :http_path, "/Content/images/"
 end
 
 ###---------------------------------------------------- Deploy Settings
@@ -101,3 +76,4 @@ activate :deploy do |deploy|
   deploy.method = :git
   deploy.user = "procload"
 end
+
