14be7d8af424d85f2db953485750311098519337
Taylor C. MacDonald <tcmacdonald@gmail.com>
2013-05-31 22:47:17 -0400

    Remove middleman smusher.

--- config.rb
+++ config.rb
@@ -58,7 +58,7 @@ helpers do
   end
 
   def recent_articles
-    @recent_articles ||= blog.articles.collect{ |a| a if a.date <= Date.today }.compact.group_by{ |a| a.date }.take(5) #.collect{ |a| a if a.date == Date.today }.compact
+    @recent_articles ||= blog.articles.collect{ |a| a if a.date <= Date.today }.compact.group_by{ |a| a.date }.take(5)
   end
 
   def todays_date
@@ -88,8 +88,8 @@ configure :build do
   
   # Compress PNGs after build
   # First: gem install middleman-smusher
-  require "middleman-smusher"
-  activate :smusher
+  # require "middleman-smusher"
+  # activate :smusher
   
   # Or use a different image path
   # set :http_path, "/Content/images/"
