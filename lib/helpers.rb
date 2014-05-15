9209de550826d9992df9669ddaf14f9cf619cb7c
Taylor C. MacDonald <tcmacdonald@gmail.com>
2014-05-15 12:26:47 -0400

    Site updated at 2014-05-15 16:26:47 UTC

--- lib/helpers.rb
+++
@@ -1,7 +0,0 @@
-module ArticleHelpers
-  def build_archive
-    ["tom", "dick", "harry"].each do |name|
-      proxy "/archives/#{Dir.pwd}/#{name}/poo.html", "/index.html", :locals => { :person_name => name }, :ignore => true
-    end
-  end
-end
--- vendor/bundle/gems/middleman-deploy-0.0.9/lib/middleman-deploy/pkg-info.rb
+++
@@ -1,6 +0,0 @@
-module Middleman
-  module Deploy
-    PACKAGE = "middleman-deploy"
-    VERSION = "0.0.9"
-  end
-end
